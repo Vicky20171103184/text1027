d26f050783cd9eec12b9ebd541c77c7ff82eeee4
Vicky71103184 <759126073@qq.com>
2018-10-27 09:49:21 +0800

    Initial Commit

---
+++ text1027/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  text1027
+//
+//  Created by s20171103184 on 2018/10/27.
+//  Copyright © 2018 s20171103184. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ text1027Tests/text1027Tests.swift
@@ -0,0 +1,34 @@
+//
+//  text1027Tests.swift
+//  text1027Tests
+//
+//  Created by s20171103184 on 2018/10/27.
+//  Copyright © 2018 s20171103184. All rights reserved.
+//
+
+import XCTest
+@testable import text1027
+
+class text1027Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
