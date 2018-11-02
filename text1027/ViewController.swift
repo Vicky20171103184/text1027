ce41ccca317c71b84ebe1035e900f26dcbcd7882
Vicky71103184 <759126073@qq.com>
2018-11-02 21:12:27 +0800

    增加需要的数字符号等

--- text1027/ViewController.swift
+++ text1027/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     var temp:Double = 0
-    
-   
+    var operatorFlag = 0
     @IBOutlet var result: UITextField!
     
+    @IBAction func button0(_ sender: Any) {
+        result.text = result.text! + "0"
+    }
     @IBAction func button1(_ sender: Any) {
         result.text = result.text! + "1"
     }
@@ -27,19 +29,71 @@ class ViewController: UIViewController {
         result.text = result.text! + "3"
     }
     
+    @IBAction func button4(_ sender: Any) {
+        result.text = result.text! + "4"
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+        result.text = result.text! + "5"
+    }
+    
+    @IBAction func button6(_ sender: Any) {
+        result.text = result.text! + "6"
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+        result.text = result.text! + "7"
+    }
+    
+    @IBAction func button8(_ sender: Any) {
+        result.text = result.text! + "8"
+    }
+    
+    @IBAction func button9(_ sender: Any) {
+        result.text = result.text! + "9"
+    }
+    
+    @IBAction func buttonPoint(_ sender: Any) {
+        result.text = result.text! + "."
+    }
     
     @IBAction func buttonadd(_ sender: Any) {
+        operatorFlag = 1
+        temp = Double(result.text!)!
+        result.text = ""
+    }
+    
+    @IBAction func buttonMultiply(_ sender: Any) {
+        operatorFlag = 1
+        temp = Double(result.text!)!
+        result.text = ""
+    }
+    
+    @IBAction func buttonDevide(_ sender: Any) {
+        operatorFlag = 1
         temp = Double(result.text!)!
         result.text = ""
     }
     
     @IBAction func buttonResult(_ sender: Any) {
-        temp = temp + Double(result.text!)!
-        result.text = "\(temp)"
+        if operatorFlag == 1
+        {
+            temp = temp + Double(result.text!)!
+            result.text = "\(temp)"
+        }
+        if operatorFlag == 2{
+            temp = temp + Double(result.text!)!
+            result.text = "\(temp)"
+        }
     }
     
-    
     @IBAction func buttonSub(_ sender: Any) {
+        operatorFlag = 2
+        temp = Double(result.text!)!
+        result.text = ""
+    }
+    
+    @IBAction func buttonMul(_ sender: Any) {
         temp = Double(result.text!)!
         result.text = ""
     }
@@ -47,9 +101,11 @@ class ViewController: UIViewController {
     @IBAction func buttonAC(_ sender: Any) {
         result.text = ""
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
